34b8b09b9867d5c8881ac453c7ece22bdc3ddcf4
Bhargav <bhargavk.k2@gmail.com>
2026-09-26 16:01:09 +0530

    design a stack with increment operation and design a stack with increment operation with min time complexity

---
+++ JavaPrgms/src/questions/StackBasedDesign/designAStackWithIncrementOperation.java
@@ -0,0 +1,93 @@
+package questions.StackBasedDesign;
+
+public class designAStackWithIncrementOperation {
+
+//    Leet code - 1381 - Medium
+
+/*
+Design a stack that supports increment operations on its elements.
+
+Implement the CustomStack class:
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack.
+void push(int x) Adds x to the top of the stack if the stack has not reached the maxSize.
+int pop() Pops and returns the top of the stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, increment all the elements in the stack.
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack stk = new CustomStack(3); // Stack is Empty []
+stk.push(1);                          // stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.push(3);                          // stack becomes [1, 2, 3]
+stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+stk.increment(5, 100);                // stack becomes [101, 102, 103]
+stk.increment(2, 100);                // stack becomes [201, 202, 103]
+stk.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+stk.pop();                            // return 202 --> Return top of the stack 202, stack becomes [201]
+stk.pop();                            // return 201 --> Return top of the stack 201, stack becomes []
+stk.pop();                            // return -1 --> Stack is empty return -1.
+
+
+Constraints:
+
+1 <= maxSize, x, k <= 1000
+0 <= val <= 100
+At most 1000 calls will be made to each method of increment, push and pop each separately.
+*/
+
+    public static void main(String[] args) {
+        designAStackWithIncrementOperation stk = new designAStackWithIncrementOperation(3);
+        stk.push(1);                          // stack becomes [1]
+        stk.push(2);                          // stack becomes [1, 2]
+        System.out.println(stk.pop());                            // return 2 --> Return top of the stack 2, stack becomes [1]
+        stk.push(2);                          // stack becomes [1, 2]
+        stk.push(3);                          // stack becomes [1, 2, 3]
+        stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+        stk.increment(5, 100);                // stack becomes [101, 102, 103]
+        stk.increment(2, 100);                // stack becomes [201, 202, 103]
+        System.out.println(stk.pop());                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+        System.out.println(stk.pop());                           // return 202 --> Return top of the stack 202, stack becomes [201]
+        System.out.println(stk.pop());        // return 201 --> Return top of the stack 201, stack becomes []
+        System.out.println(stk.pop());        // return -1 --> Stack is empty return -1.
+    }
+
+    int[] stack;
+    int maxSize;
+    int index;
+    public designAStackWithIncrementOperation(int maxSize){
+        this.maxSize = maxSize;
+        stack = new int[maxSize];
+        index = -1;
+    }
+
+    public void push(int x) {
+        if(index == maxSize-1) return;
+        index++;
+        stack[index] = x;
+    }
+
+    public int pop() {
+        if(index == -1) return -1;
+        int val = stack[index];
+        stack[index] = 0;
+        index--;
+        return val;
+    }
+
+    public void increment(int k, int val) {
+        k = Math.min(k, index+1); //because k is size or number of elements to be updated and index is the current length of that stack.
+        //if k is higher than index+1, which means all elements are to be updated and not k
+        //if index+1 is higher, than only k elements are to be updated
+        for(int i=0; i<k;i++){
+            stack[i] += val;
+        }
+    }
+}
